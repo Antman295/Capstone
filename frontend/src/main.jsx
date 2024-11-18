214576b10ad433a90297c99f5b2bc82c4f1c9315
Antman295 <antjosh14@gmail.com>
2024-11-18 12:25:25 -0500

    Front End Started
    Started working on the frontend of my app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BroswerRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BroswerRouter>
     <App />
+    </BroswerRouter>
   </StrictMode>,
 )
