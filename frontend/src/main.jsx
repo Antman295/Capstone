dd121855e93be769a89db8ad10f92a8fdc211a59
Antman295 <antjosh14@gmail.com>
2024-11-18 12:27:06 -0500

    Update main.jsx
    I just can't spell -_-

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BroswerRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BroswerRouter>
+    <BrowserRouter>
     <App />
-    </BroswerRouter>
-  </StrictMode>,
+    </BrowserRouter>
+  </StrictMode>
 )
