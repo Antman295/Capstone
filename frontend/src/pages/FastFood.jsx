92d6464bb8d777b1e33896ccf536d610b4f8614d
Antman295 <antjosh14@gmail.com>
2024-11-23 14:35:29 -0500

    Starting on API section
    Started on the styling for this, but something went wrong and the background for this is now showing up for the homepage. I'll figure that out in due time

--- frontend/src/pages/FastFood.jsx
+++ frontend/src/pages/FastFood.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
+import '../styles/FastFood.css'
 
 export default function FastFood() {
     return (
-        <>
+        <div class = "fastfood">
         <h1>BE HEALTHY AND MAKE SOMETHING AT HOME</h1>
         <nav>
         <Link to={'/'}><button>Go Back</button></Link>
     </nav>
-    </>
+    </div>
     )
 }
